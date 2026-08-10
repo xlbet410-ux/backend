88b8c3ccca7d42fdefabb0f19c0276e0bac92936
Thoai <thoaichak63@gmail.com>
2026-08-10 13:09:03 +0600

    fix wallet relations

---
+++ prisma/migrations/20260810070840_fix_deposit_withdrawal_relations/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "cash_transactions_approved_by_agent_id_idx";
+
+-- DropIndex
+DROP INDEX "cash_transactions_payment_account_id_idx";
+
+-- DropIndex
+DROP INDEX "cash_transactions_user_id_idx";
+
+-- DropIndex
+DROP INDEX "idx_kyc_verifications_status";
